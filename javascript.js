70edc2c09f43e585f0b6165ea697a40a25b9290c
dungdul <178075100+dungdul@users.noreply.github.com>
2026-02-18 20:05:09 +0900

    Implement a function to display books' info

--- javascript.js
+++ javascript.js
@@ -1,4 +1,10 @@
 const myLibrary = [];
+myLibrary.push({
+  title: "Harry",
+  author: "JK",
+  pages: 200,
+  read: false
+})
 
 function Book(inputObject) {
   this.id = crypto.randomUUID();
@@ -12,5 +18,28 @@ function addBookToLibrary(inputObject) {
 }
 
 function displayLibrary() {
-  
+  const libraryDiv = document.querySelector("#library");
+  myLibrary.forEach(book => {
+    libraryDiv.append(createBookCard(book));
+  })
+}
+
+function createBookCard(book) {
+  // Create card's child elements
+  let title = document.createElement("h2");
+  title.classList.add("title");
+  title.textContent = book.title;
+  let author = document.createElement("p");
+  author.textContent = `Author: ${book.author}`;
+  let pages = document.createElement("p");
+  pages.textContent = `${book.pages} pages`;
+  let readStatus = document.createElement("p");
+  readStatus.textContent = book.read ? "read" : "unread";
+
+  // Create card and append children to it
+  let card = document.createElement("div");
+  card.classList.add("card");
+  card.append(title, author, pages, readStatus);
+
+  return card;
 }
