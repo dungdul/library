81f86bb5e6ae9c1f30875e52c9740d04864fdedc
dungdul <178075100+dungdul@users.noreply.github.com>
2026-02-19 15:47:01 +0900

    Implement js to add a new book

--- javascript.js
+++ javascript.js
@@ -6,6 +6,14 @@ myLibrary.push({
   read: false
 })
 
+const dialog = document.querySelector("#add-book-dialog");
+const textInputs = document.querySelectorAll('input[type="text"]');
+const numberInputs = document.querySelectorAll('input[type="number"]');
+const radioButtons = document.querySelectorAll('input[type="radio"]');
+const cancelButton = document.querySelector('#cancel');
+const confirmButton = document.querySelector('#confirm');
+const libraryDiv = document.querySelector("#library");
+
 function Book(inputObject) {
   this.id = crypto.randomUUID();
   Object.assign(this, inputObject);
@@ -18,7 +26,7 @@ function addBookToLibrary(inputObject) {
 }
 
 function displayLibrary() {
-  const libraryDiv = document.querySelector("#library");
+  libraryDiv.innerHTML = "";
   myLibrary.forEach(book => {
     libraryDiv.append(createBookCard(book));
   })
@@ -43,3 +51,43 @@ function createBookCard(book) {
 
   return card;
 }
+
+function getInputValues() {
+  let inputObject = {};
+  textInputs.forEach(input => {
+    inputObject[input.id] = input.value;
+  })
+  numberInputs.forEach(input => {
+    inputObject[input.id] = +input.value;
+  })
+  let read = Array.from(radioButtons).filter(button => button.checked)[0].value;
+  inputObject.read = read === "yes" ? true : false;
+  return inputObject;
+}
+
+cancelButton.addEventListener("click", event => {
+  event.preventDefault();
+  dialog.close();
+})
+
+// When user adds a new book, add the book to the array, clear input fields, and update the display
+confirmButton.addEventListener("click", event => {
+  event.preventDefault();
+  addBookToLibrary(getInputValues());
+
+  // Clear all input fields
+  textInputs.forEach(input => {
+    input.value = "";
+  })
+  numberInputs.forEach(input => {
+    input.value = "";
+  })
+  radioButtons.forEach(button => {
+    button.checked = false;
+  })
+
+  dialog.close();
+  displayLibrary();
+})
+
+displayLibrary();
